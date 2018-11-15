8eaf2e88ecf442d42eb5decbc64c570c00824e5f
pkochar1307 <44370199+pkochar1307@users.noreply.github.com>
2018-11-15 18:33:20 +0530

    Add files via upload

---
+++ Spring/SpringJpaMvc/src/com/cg/springmvcone/controller/MyController.java
@@ -0,0 +1,93 @@
+package com.cg.springmvcone.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cg.springmvcone.dto.Employee;
+
+import com.cg.springmvcone.service.IEmployeeService;
+import com.cg.springmvcone.service.EmployeeServiceImpl;
+
+@Controller
+public class MyController {
+
+	@Autowired
+	IEmployeeService employeeService;
+	@RequestMapping(value="all",method=RequestMethod.GET)
+	public String getAll(){
+		return "home";
+	}
+	
+	@RequestMapping(value="add",method=RequestMethod.GET)
+	public String addEmployee(@ModelAttribute("my") Employee emp,Map<String,Object> model){
+		List<String> myDeg = new ArrayList<>();
+		myDeg.add("software engg");
+		myDeg.add("sr consultant");
+		myDeg.add("manager");
+		model.put("deg", myDeg);
+		return "addemployee";
+		
+	}
+	
+	@RequestMapping(value="insertdata",method=RequestMethod.POST)
+	public ModelAndView insertEmployee(@Valid@ModelAttribute("my") Employee emp, BindingResult result, Map<String,Object> model){
+		int id=0;
+		if(result.hasErrors()){
+			List<String> myDeg = new ArrayList<>();
+			myDeg.add("software engg");
+			myDeg.add("sr consultant");
+			myDeg.add("manager");
+			model.put("deg", myDeg);
+			return new ModelAndView("addEmployee");
+		}else{
+			id=employeeService.addEmployeeData(emp);
+		}
+		return new ModelAndView("sucess","edata",id);
+	}
+	
+	@RequestMapping(value="show",method=RequestMethod.GET)
+		public ModelAndView showAllEmployee(){
+			List<Employee> myAllData = employeeService.showAllEmployee();
+			return new ModelAndView("showall","temp",myAllData);
+		}
+
+	@RequestMapping(value="delete",method=RequestMethod.GET)
+	public String deleteEmployee(){
+		return "deleteemployee";
+	}
+	
+
+	@RequestMapping(value="dodelete",method=RequestMethod.GET)
+	public String employeeDelete(@RequestParam("eid") int id){
+		employeeService.deleteEmployee(id);
+		return "delete";
+	}
+	
+	@RequestMapping(value="update",method=RequestMethod.GET)
+	public String updateEmployee(@ModelAttribute("my") Employee emp){
+		return "updateemployee";
+	}
+	
+	@RequestMapping(value="doupdate",method=RequestMethod.GET)
+	public String employeeUpdate(@ModelAttribute("my") Employee emp){
+		employeeService.updateEmployee(emp.getEmpId());
+		return "update";
+	}
+	
+	@RequestMapping(value="search",method=RequestMethod.GET)
+	public String searchEmployee(){
+		return "searchemployee";
+	}
+}
---
+++ Spring/SpringJpaMvc/src/com/cg/springmvcone/dao/EmployeeDaoImpl.java
@@ -0,0 +1,55 @@
+package com.cg.springmvcone.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+
+import com.cg.springmvcone.dto.Employee;
+@Repository("employeedao")
+public class EmployeeDaoImpl implements IEmployeeDao{
+    
+	@PersistenceContext
+	EntityManager entityManager;
+	@Override
+	public int addEmployeeData(Employee emp) {
+		// TODO Auto-generated method stub
+		entityManager.persist(emp);
+		entityManager.flush();
+		return emp.getEmpId();
+	}
+
+	@Override
+	public List<Employee> showAllEmployee() {
+		// TODO Auto-generated method stub
+		Query queryOne = entityManager.createQuery("FROM Employee");
+		List<Employee> myList= queryOne.getResultList();
+		return myList;
+	}
+
+	@Override
+	public void deleteEmployee(int empId) {
+		// TODO Auto-generated method stub
+		Query queryTwo = entityManager.createQuery("DELETE FROM Employee "+"WHERE empId=:eid");
+		queryTwo.setParameter("eid",empId);
+		queryTwo.executeUpdate();
+	}
+
+	@Override
+	public void updateEmployee(int empId) {
+		// TODO Auto-generated method stub
+		Query queryThree = entityManager.createQuery("UPDATE Employee SET empName='Bhuvana',empSalary=200 WHERE empId=:eid");
+		queryThree.setParameter("eid",empId);
+		queryThree.executeUpdate();
+	}
+
+	@Override
+	public Employee searchEmployee(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
